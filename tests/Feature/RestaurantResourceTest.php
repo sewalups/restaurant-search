14ce67eadcc35143b9ded65a7716c5b72b7a36aa
stevensewalu <ssewalu@projectcode.ug>
2023-03-21 09:25:10 +0300

    tests and readme work

--- tests/Feature/RestaurantResourceTest.php
+++ tests/Feature/RestaurantResourceTest.php
@@ -13,9 +13,19 @@ class RestaurantResourceTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_the_application_returns_a_successful_response_on_accessing_all_restaurants_by_supplied_location_and_term()
     {
-        $response = $this->get('/');
+        $location = "New York";
+        $term = "pizza";
+        $response = $this->get('/api/restaurants?location='.$location.'&term='.$term);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_the_application_returns_a_successful_response_on_accessing_a_single_restaurant_by_yelp_id()
+    {
+        $id = "n601y41nOORUePsM7KsuaA"; //not a very efficient way of doing this, coz i cannot mock it since am not using internal state for this app
+        $response = $this->get('/api/restaurants/'.$id);
 
         $response->assertStatus(200);
     }
